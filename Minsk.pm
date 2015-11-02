5dc44761f41b9cd3f5f5dbf4cff00327e5d6825e
Michal Špaček <tupinek@gmail.com>
2015-11-02 18:38:39 +0100

    Add EXAMPLE5 section to doc.

--- Minsk.pm
+++ Minsk.pm
@@ -219,6 +219,55 @@ For more information about Minsk imetro map, click L<here|https://en.wikipedia.o
  # Аўтазаводская лінія
  # Маскоўская лінія
 
+=head1 EXAMPLE5
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(decode_utf8 encode_utf8);
+ use Map::Tube::Minsk;
+
+ # Arguments.
+ if (@ARGV < 1) {
+         print STDERR "Usage: $0 line\n";
+         exit 1;
+ }
+ my $line = decode_utf8($ARGV[0]);
+
+ # Object.
+ my $obj = Map::Tube::Minsk->new;
+
+ # Get stations for line.
+ my $stations_ar = $obj->get_stations($line);
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } @{$stations_ar};
+
+ # Output:
+ # Usage: __PROG__ line
+
+ # Output with 'foo' argument.
+ # Map::Tube::get_stations(): ERROR: Invalid Line Name [foo]. (status: 105) file __PROG__ on line __LINE__
+
+ # Output with 'Маскоўская лінія' argument.
+ # Уручча
+ # Барысаўскі тракт
+ # Усход
+ # Маскоўская
+ # Парк Чалюскінцаў
+ # Акадэмія навук
+ # Плошча Якуба Коласа
+ # Плошча Перамогі
+ # Кастрычніцкая
+ # Плошча Леніна
+ # Інстытут культуры
+ # Грушаўка
+ # Міхалова
+ # Пятроўшчына
+ # Малінаўка
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
