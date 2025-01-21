1723f1875ae1c815e3bf12a67f8ad6b32c1c6aae
Michal Josef Špaček <tupinek@gmail.com>
2025-01-21 18:06:18 +0100

    Regen image

--- Minsk.pm
+++ Minsk.pm
@@ -214,7 +214,7 @@ Returns string with XML.
  system "ls -l Minsk.png";
 
  # Output like:
- # -rw-r--r-- 1 skim skim 85988 Jan  4 11:32 Minsk.png
+ # -rw-r--r-- 1 skim skim 94891 21. led 18.04 Minsk.png
 
 =begin html
 
